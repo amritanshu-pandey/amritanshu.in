ba878e05c9a0fc5a56f68ee46bc2cbf3d3be604c
Amritanshu Pandey <email@amritanshu.in>
2024-10-14 23:25:10 +1100

    add otel metrics

--- goblog/backend/backend.go
+++ goblog/backend/backend.go
@@ -8,21 +8,24 @@ import (
 
 	"amritanshu.in/goblog/md"
 	"amritanshu.in/goblog/views"
-	"github.com/a-h/templ"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/sdk/metric"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
 func initMeter() (*sdkmetric.MeterProvider, error) {
-	exp, err := stdoutmetric.New()
+	exp, err := prometheus.New()
+	// exp, err := stdoutmetric.New(stdoutmetric.WithPrettyPrint())
 	if err != nil {
 		return nil, err
 	}
 
-	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exp)))
+	// mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exp, metric.WithInterval(time.Second * 5))))
+	mp := sdkmetric.NewMeterProvider(metric.WithReader(exp))
 	otel.SetMeterProvider(mp)
 	return mp, nil
 }
@@ -48,29 +51,33 @@ func RunServer(markdownPath string, assetsDir string, serverPort int, serverBind
 		}
 	}()
 
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		handler := otelhttp.NewHandler(otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc)), "")
+		fmt.Printf("Mux is handling connection for pattern: %s\n", pattern)
+		mux.Handle(pattern, handler)
+	}
+
 	// Index Page
-	http.Handle("/", otelhttp.NewHandler(otelhttp.WithRouteTag("/", templ.Handler(views.Index(markdownPosts, sortedTitles))), ""))
+	handleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		views.Index(markdownPosts, sortedTitles).Render(r.Context(), w)
+	})
 
 	// Static assets
 	staticFs := http.FileServer(http.Dir(assetsDir))
-	http.Handle("/assets/", otelhttp.NewHandler(otelhttp.WithRouteTag("/assets", http.StripPrefix("/assets/", staticFs)), ""))
-
-	// Articles
-	// http.HandleFunc("/article/{slug}", func(w http.ResponseWriter, r *http.Request) {
-	// 	slug := r.PathValue("slug")
-	// 	views.Article(markdownPosts[slug]).Render(r.Context(), w)
-	// })
-
-	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-		handler:= otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
-		mux.Handle(pattern, handler)
-	}
+	handleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
+		http.StripPrefix("/assets", staticFs).ServeHTTP(w, r)
+	})
 
 	handleFunc("/article/{slug}", func(w http.ResponseWriter, r *http.Request) {
 		slug := r.PathValue("slug")
 		views.Article(markdownPosts[slug]).Render(r.Context(), w)
 	})
 
-	http.ListenAndServe(fmt.Sprintf("%s:%d", serverBindAddr, serverPort), nil)
+	handleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		promhttp.Handler().ServeHTTP(w, r)
+	})
+
+	fmt.Printf("Starting server on %s:%d\n", serverBindAddr, serverPort)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", serverBindAddr, serverPort), mux)
 	return nil
 }
