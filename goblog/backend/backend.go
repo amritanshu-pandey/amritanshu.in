40b894a34b914ab7d63df18527bb52d0a539f16b
Amritanshu Pandey <email@amritanshu.in>
2024-10-14 22:15:54 +1100

    add otel support

--- goblog/backend/backend.go
+++ goblog/backend/backend.go
@@ -1,15 +1,34 @@
 package backend
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
 	"amritanshu.in/goblog/md"
 	"amritanshu.in/goblog/views"
 	"github.com/a-h/templ"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
+func initMeter() (*sdkmetric.MeterProvider, error) {
+	exp, err := stdoutmetric.New()
+	if err != nil {
+		return nil, err
+	}
+
+	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exp)))
+	otel.SetMeterProvider(mp)
+	return mp, nil
+}
+
 func RunServer(markdownPath string, assetsDir string, serverPort int, serverBindAddr string) error {
+	mux := http.NewServeMux()
 	markdownPosts, err := md.ActivePosts(markdownPath)
 	if err != nil {
 		return err
@@ -19,15 +38,39 @@ func RunServer(markdownPath string, assetsDir string, serverPort int, serverBind
 		return err
 	}
 
-	staticFs := http.FileServer(http.Dir(assetsDir))
+	mp, err := initMeter()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := mp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down meter provider: %v", err)
+		}
+	}()
 
-	http.Handle("/", templ.Handler(views.Index(markdownPosts, sortedTitles)))
-	fmt.Println(assetsDir)
-	http.Handle("/assets/", http.StripPrefix("/assets/", staticFs))
-	http.HandleFunc("/article/{slug}", func(w http.ResponseWriter, r *http.Request) {
+	// Index Page
+	http.Handle("/", otelhttp.NewHandler(otelhttp.WithRouteTag("/", templ.Handler(views.Index(markdownPosts, sortedTitles))), ""))
+
+	// Static assets
+	staticFs := http.FileServer(http.Dir(assetsDir))
+	http.Handle("/assets/", otelhttp.NewHandler(otelhttp.WithRouteTag("/assets", http.StripPrefix("/assets/", staticFs)), ""))
+
+	// Articles
+	// http.HandleFunc("/article/{slug}", func(w http.ResponseWriter, r *http.Request) {
+	// 	slug := r.PathValue("slug")
+	// 	views.Article(markdownPosts[slug]).Render(r.Context(), w)
+	// })
+
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		handler:= otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+
+	handleFunc("/article/{slug}", func(w http.ResponseWriter, r *http.Request) {
 		slug := r.PathValue("slug")
 		views.Article(markdownPosts[slug]).Render(r.Context(), w)
 	})
+
 	http.ListenAndServe(fmt.Sprintf("%s:%d", serverBindAddr, serverPort), nil)
 	return nil
 }
--- goblog/go.mod
+++ goblog/go.mod
@@ -5,5 +5,26 @@ go 1.22.6
 require (
 	github.com/a-h/templ v0.2.778
 	github.com/gomarkdown/markdown v0.0.0-20240930133441-72d49d9543d8
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.55.0
+	go.opentelemetry.io/otel v1.31.0
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v1.31.0
+	go.opentelemetry.io/otel/sdk/metric v1.31.0
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	go.opentelemetry.io/otel/sdk v1.31.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v1.31.0 // indirect
+	go.opentelemetry.io/otel/trace v1.31.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
